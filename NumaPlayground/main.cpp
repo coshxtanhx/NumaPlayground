6cebfa703189643bd0fb9729b034f964b095f2eb
Cho Yong Joon <yzz1108j@gmail.com>
2026-01-17 12:25:41 +0900

    feat: 3rd thread func

--- NumaPlayground/main.cpp
+++ NumaPlayground/main.cpp
@@ -12,6 +12,7 @@ struct alignas(std::hardware_destructive_interference_size)
 std::array<AtomicInt, 6000> arr;
 numa::Array<AtomicInt, 3000> narr1{ 0 };
 numa::Array<AtomicInt, 3000> narr2{ 1 };
+numa::Array<AtomicInt, 3000> narr3{ 0 };
 
 const auto kNumThread{ static_cast<int>(std::thread::hardware_concurrency()) };
 constexpr auto kLoop{ 360'000'000 };
@@ -26,6 +27,19 @@ void ThreadFuncA(int thread_id)
 void ThreadFuncB(int thread_id)
 {
 	std::array<numa::Array<AtomicInt, 3000>*, 2> arrs{ &narr1, &narr2 };
+
+	numa::SetCPUAffinity(thread_id);
+
+	auto& arr{ *arrs[(thread_id / 10) % 2] };
+
+	for (int i = 0; i < kLoop / kNumThread; ++i) {
+		arr[Random::Get(0, arr.Get().size() - 1)] += 1;
+	}
+}
+
+void ThreadFuncC(int thread_id)
+{
+	std::array<numa::Array<AtomicInt, 3000>*, 2> arrs{ &narr1, &narr3 };
 
 	numa::SetCPUAffinity(thread_id);
 
@@ -68,10 +82,24 @@ int main()
 
 	compat::Print("{} sec\n", stopwatch.GetDuration());
 
+	threads.clear();
+
+	stopwatch.Start();
+
+	for (int i = 0; i < kNumThread; ++i) {
+		threads.emplace_back(ThreadFuncC, i);
+	}
+
+	for (auto& thread : threads) {
+		thread.join();
+	}
+
+	compat::Print("{} sec\n", stopwatch.GetDuration());
+
 	compat::Print("\n");
 
 	for (int i = 0; i < 5; ++i) {
-		auto r = Random::Get(0, 2);
+		auto r = Random::Get(0, 4);
 		auto id = Random::Get(0, 5999);
 
 		switch (r) {
@@ -83,6 +111,9 @@ int main()
 				break;
 			case 2:
 				compat::Print("{} ", narr2[id / 2].load());
+				break;
+			case 3:
+				compat::Print("{} ", narr3[id / 2].load());
 				break;
 		}
 	}
